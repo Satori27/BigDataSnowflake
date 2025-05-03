0e5aa8d14e360d395101532e1b0289de17da40e3
Davron <ddiyorovprogram@gmail.com>
2025-05-03 18:58:14 +0300

    Сделал лабу

---
+++ initdb/02_import_all_csv.sql
@@ -0,0 +1,13 @@
+-- DML/import_all_csv.sql
+
+-- Повторите для каждого файла
+COPY staging_sales FROM '/data/MOCK_DATA (1).csv' WITH CSV HEADER;
+COPY staging_sales FROM '/data/MOCK_DATA (2).csv' WITH CSV HEADER;
+COPY staging_sales FROM '/data/MOCK_DATA (3).csv' WITH CSV HEADER;
+COPY staging_sales FROM '/data/MOCK_DATA (4).csv' WITH CSV HEADER;
+COPY staging_sales FROM '/data/MOCK_DATA (5).csv' WITH CSV HEADER;
+COPY staging_sales FROM '/data/MOCK_DATA (6).csv' WITH CSV HEADER;
+COPY staging_sales FROM '/data/MOCK_DATA (7).csv' WITH CSV HEADER;
+COPY staging_sales FROM '/data/MOCK_DATA (8).csv' WITH CSV HEADER;
+COPY staging_sales FROM '/data/MOCK_DATA (9).csv' WITH CSV HEADER;
+COPY staging_sales FROM '/data/MOCK_DATA.csv' WITH CSV HEADER;
